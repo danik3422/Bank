9262e3a1f0026a77dd2a7cc3661e18053f707081
danik3422 <42092279+danik3422@users.noreply.github.com>
2020-06-11 18:02:16 +0200

    Init
    init

---
+++ Bankomat/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Bankomat
+{
+    partial class Bankomat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberOfCard = new System.Windows.Forms.TextBox();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Podaj numer Konta:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numberOfCard
+            // 
+            this.numberOfCard.Location = new System.Drawing.Point(212, 172);
+            this.numberOfCard.Name = "numberOfCard";
+            this.numberOfCard.Size = new System.Drawing.Size(349, 20);
+            this.numberOfCard.TabIndex = 1;
+            this.numberOfCard.TextChanged += new System.EventHandler(this.numberOfCard_TextChanged);
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(254, 216);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(208, 85);
+            this.btnstart.TabIndex = 2;
+            this.btnstart.Text = "Wlóż kartę";
+            this.btnstart.UseVisualStyleBackColor = true;
+            // 
+            // Bankomat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.numberOfCard);
+            this.Controls.Add(this.label1);
+            this.Name = "Bankomat";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Bankomat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numberOfCard;
+        private System.Windows.Forms.Button btnstart;
+    }
+}
+
---
+++ Bankomat/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bankomat
+{
+    public partial class Bankomat : Form
+    {
+        public Bankomat()
+        {
+            CenterToScreen();
+            InitializeComponent();
+        }
+
+        private void Bankomat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numberOfCard_TextChanged(object sender, EventArgs e)
+        {
+            numberOfCard.MaxLength = 31;
+        }
+    }
+}
